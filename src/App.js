2be7a212b596131651624e41222c9e7b7903b807
Muhaimin Jobayer <mahmudmuhaimin@yahoo.com>
2021-09-30 22:55:41 -0500

    created static components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import FilterableList from './FilterableList/FilterableList';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar/>
+        <FilterableList files={this.props.files} />
+      </div>
+    )
+  }
 }
-
 export default App;
